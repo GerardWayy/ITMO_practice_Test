511bfe5d119015d19579025cc941c8999cabb1f0
GerardWayy <ingeneer196@mail.ru>
2024-12-21 22:53:36 +0300

    Practice 2

---
+++ src/practice2/Calculator.java
@@ -0,0 +1,42 @@
+package practice2;
+
+public class Calculator {
+    public void sum(int a, int b) {
+
+    }
+
+    public void sum(long a, long b) {
+    }
+
+    public void sum(double a, double c) {
+    }
+
+    public void minus(long a, long b) {
+    }
+
+    public void minus(int a, int b) {
+    }
+
+    public void minus(double a, double b) {
+    }
+
+    public void del(long a, long b) {
+    }
+
+    public void del(int a, int b) {
+    }
+
+    public void del(double a, double b) {
+    }
+
+    public void mult(double a, double b) {
+    }
+
+    public void mult(int a, int b) {
+    }
+
+    public void mult(long a, long b) {
+    }
+
+
+}
---
+++ src/practice2/Cat.java
@@ -0,0 +1,19 @@
+package practice2;
+
+public class Cat {
+    int age;
+    String name;
+    int weight;
+    String color;
+
+    public Cat() {
+
+    }
+
+    public Cat(int age, String name, int weight, String color) {
+        this.age = age;
+        this.name = name;
+        this.weight = weight;
+        this.color = color;
+    }
+}
