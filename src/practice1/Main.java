4dbc9156da5ee8fe4437f692ce8d6e11f331e260
GerardWayy <ingeneer196@mail.ru>
2024-12-21 13:26:57 +0300

    Practice 1

--- src/practice1/Main.java
+++ src/practice1/Main.java
@@ -1,7 +1,42 @@
 package practice1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("d");
+        System.out.println("Я");
+        System.out.println("хорошо");
+        System.out.println("знаю");
+        System.out.println("Java.");
+
+        int number1 = (46 + 10) * (10 / 3);
+        int number2 = (29) * (4) * (-15);
+
+        int number = 10500;
+        int result =  (number / 10) / 10;
+
+        System.out.println(result);
+
+        double result1 = 3.6 * 4.1 * 5.9;
+
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0;i<3;i++ ){
+            int b = scanner.nextInt();
+            System.out.println(b);
+        }
+        // while (scanner.hasNextInt()) {
+       //     int b = scanner.nextInt();
+       //     System.out.println(b);
+       // }
+        
+        int b = 1;
+        
+        if(b%2==0 && b > 100){
+            System.out.println("Выход за пределы диапазона");
+        }else if (b%2!=0) {
+            System.out.println("Нечетное");
+        }else if(b%2==0){
+            System.out.println("Четное");
+        }
     }
 }
