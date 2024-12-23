64c258f0df608a905295e438bae0cfa5aa97b658
GerardWayy <ingeneer196@mail.ru>
2024-12-23 12:52:09 +0300

    Practice 2

--- src/practice2/Calculator.java
+++ src/practice2/Calculator.java
@@ -1,42 +1,89 @@
 package practice2;
 
 public class Calculator {
-    public void sum(int a, int b) {
 
+    public static double sum(int a, long b) {
+        return a + b;
     }
 
-    public void sum(long a, long b) {
+    public static double test(double a, int b) {
+        return a + b;
     }
 
-    public void sum(double a, double c) {
+    public static double minus(double a, int b) {
+        return a - b;
     }
 
-    public void minus(long a, long b) {
+    public static double minus(double a, long b) {
+        return a - b;
     }
 
-    public void minus(int a, int b) {
+    public static double del(double a, long b) {
+        return a / b;
     }
 
-    public void minus(double a, double b) {
+    public static double del(int a, long b) {
+        return a / b;
     }
 
-    public void del(long a, long b) {
+    public static double mult(double a, long b) {
+        return a * b;
     }
 
-    public void del(int a, int b) {
+    public static double mult(double a, int b) {
+        return a * b;
     }
 
-    public void del(double a, double b) {
-    }
+    /*  public void sum(int a, int b) {
+          int sum = a + b;
+      }
 
-    public void mult(double a, double b) {
-    }
+      public void sum(long a, long b) {
+          long sum = a + b;
+      }
 
-    public void mult(int a, int b) {
-    }
+      public void sum(double a, double c) {
+          double sum = a + c;
+      }
 
-    public void mult(long a, long b) {
-    }
+      public void minus(long a, long b) {
+
+          long minus = a - b;
+      }
+
+      public void minus(int a, int b) {
+          long minus = a - b;
+      }
+
+      public void minus(double a, double b) {
+          double minus = a - b;
+      }
+
+      public void del(long a, long b) {
+
+          long del = a / b;
+      }
+
+      public void del(int a, int b) {
+          long del = a / b;
+      }
+
+      public void del(double a, double b) {
+          double del = a / b;
+      }
+
+      public void mult(double a, double b) {
+          double mult = a * b;
+      }
+
+      public void mult(int a, int b) {
+          long mult = (long) a * b;
+      }
+
+      public void mult(long a, long b) {
+          long mult = a * b;
+      }
+     */
 
 
 }
