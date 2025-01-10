06c71820a1d71e57f63c8a78fee0b88fd7b5b938
GerardWayy <ingeneer196@mail.ru>
2025-01-10 22:54:03 +0300

    Git init

---
+++ src/practice5/Words.java
@@ -0,0 +1,65 @@
+package practice5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Words {
+
+    private static String longestWord(String text) {
+        String[] word = text.split(" ");
+        Arrays.sort(word);
+        int x = 0;
+        String longest = " ";
+        for (String i : word) {
+            if (i.length() > x) {
+                x = i.length();
+                longest = i;
+            }
+        }
+        System.out.println(longest);
+        return longest;
+    }
+
+    public static boolean isPalindrome(String s) {
+        return s.equals((new StringBuilder(s)).reverse().toString());
+
+    }
+
+    public static void replaceText() {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine().replaceAll("бяка", "[вырезано цензурой]");
+        System.out.println(text);
+    }
+
+    public static int count(String text1, String text2) {
+        return (text1.length() - text1.replace(text2, "").length()) / text2.length();
+
+    }
+
+    public static String reversWords(String words) {
+        String rever = String.valueOf(new StringBuilder(words).reverse());
+        String result = "";
+        for (String x : rever.split(" ")) {
+            result = String.format("%s%s%s", x, " ", result);
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Words.reversWords("CAT DOG"));
+        Words.longestWord("fewfeowj wefewf efew few ewfew e");
+        Words.isPalindrome("faffff");
+        Words.replaceText();
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.next();
+        String word1 = scanner.next();
+        int count1 = count(word, word1);
+        System.out.println(count1);
+
+        scanner.close();
+    }
+}
